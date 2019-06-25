afdcdcde5b32df071dfabf805f101b6b42e45e3b
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-24 20:57:29 -0700

    thanks lance for help with error

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const map = require('./array-modules');
 
 const myArray = [2, 1, 20, 4];
 
-function square(num) {
+function square1(num) {
     return num * num;
 }
 
-const rambo = map(myArray, square());
+const rambo = map(myArray, square1);
 
 console.log(rambo);
