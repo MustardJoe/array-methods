22d332239b4eeb794fe39e366e45c31a6f3b9698
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-24 20:57:01 -0700

    got map thing working

--- array-modules.js
+++ array-modules.js
@@ -2,10 +2,10 @@ function map(arr, callback) {
     const mappedArray = [];
     for(let i = 0; i < arr.length; i++) {
         // use callback for each index, return new array, push to array
-        const mappedValue = callback(i);
+        const mappedValue = callback(arr[i]);
         mappedArray.push(mappedValue);
     }
     return mappedArray;
-}
+};
 
 module.exports = map;
