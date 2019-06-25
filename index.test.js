753c39b63b05110c54dbad05cb9fe962c7ae87d8
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-25 10:16:09 -0700

    tdded filter array func

--- index.test.js
+++ index.test.js
@@ -1,13 +1,29 @@
-const map = require('./array-modules');
+const { map, filter } = require('./array-modules');
 
-function double(num) {
-    return num * 2;
-}
 
 describe('my hand made array methods', () => {
     it('map test using double', () => {
         const array1 = [1, 2, 4, 8];
-        const array2 = map(array1, double);
-        expect(array2).toEqual([2, 4, 8, 16]);
+        
+        function double(num) {
+            return num * 2;
+        } //double => 
+
+        const mappedArray = map(array1, double);
+        expect(mappedArray).toEqual([2, 4, 8, 16]);
+    });
+
+    it('returns an array that has been filtered', () => {
+        const array3 = [2, 6, 80, 100, 7, 33];
+
+        function greaterT10(num) {
+            if(num >= 10) {
+                return true;
+            }
+        }
+
+        const filteredArray = filter(array3, greaterT10);
+
+        expect(filteredArray).toEqual([80, 100, 33]);
     });
 });
