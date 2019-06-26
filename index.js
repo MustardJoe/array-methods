1247ae4dec9a9eca2548df48f7298fac3c7172af
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-26 16:25:53 -0700

    fixed linting error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const map = require('./array-modules');
 const myArray = [2, 1, 20, 4];
 
 function square1(num) {
-    return num * num;
+  return num * num;
 }
 
 const rambo = map(myArray, square1);
