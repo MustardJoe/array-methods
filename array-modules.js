56761dd97d522cd2f73a9c4e3f8291494863995d
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-26 16:25:42 -0700

    fixed mostly linting errors and one logic error

--- array-modules.js
+++ array-modules.js
@@ -1,51 +1,51 @@
 function map(arr, callback) {
-    const mappedArray = [];
-    for(let i = 0; i < arr.length; i++) {
-        // use callback for each index, return new array, push to array
-        const mappedValue = callback(arr[i]);
-        mappedArray.push(mappedValue);
-    };
-    return mappedArray;
-};
+  const mappedArray = [];
+  for(let i = 0; i < arr.length; i++) {
+    // use callback for each index, return new array, push to array
+    const mappedValue = callback(arr[i]);
+    mappedArray.push(mappedValue);
+  }
+  return mappedArray;
+}
 
 function filter(arr, callback) {
-    const filteredArray = [];
-    for(let i = 0; i < arr.length; i++) {
-        if(callback(arr[i])) {
-            filteredArray.push(arr[i]);
-        }
-    };
-    return filteredArray;
-};
+  const filteredArray = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(callback(arr[i])) {
+      filteredArray.push(arr[i]);
+    }
+  }
+  return filteredArray;
+}
 
 function findIndex(arr, callback) {
-    for (let i = 0; i < arr.length; i++) {
-        if(callback(arr[i])) {
-            let returnNumb = i;
-            return returnNumb;
-        } 
-    };
-    let returnNumb = -1;
-    return returnNumb;  
-};
+  for(let i = 0; i < arr.length; i++) {
+    if(callback(arr[i])) {
+      let returnNumb = i;
+      return returnNumb;
+    } 
+  }
+  let returnNumb = -1;
+  return returnNumb;  
+}
 
 function reduce(arr, callback, initialVal) {
-    let acc = initialVal;
-    let i = 0;
+  let acc = initialVal;
+  let i = 0;
 
-    if(initialVal === undefined) {
-        let acc = arr[0];
-        i = 1;
-    }
-    for (i; i < arr.length; i++) {
-        acc = callback(acc, arr[i]);    
-    }
-    return acc;
+  if(initialVal === undefined) {
+    acc = arr[0];
+    i = 1;
+  }
+  for(i; i < arr.length; i++) {
+    acc = callback(acc, arr[i]);    
+  }
+  return acc;
 }
 
 module.exports = {
-    map,
-    filter,
-    findIndex,
-    reduce,
+  map,
+  filter,
+  findIndex,
+  reduce,
 };
